adbaad9b0c8de874654abfdd972435675a2e8e8c
thibault <thibault.rey@akretion.com>
2022-03-14 12:13:12 +0100

    [IMP] Support response Type on ERP fetch

--- ErpFetch.js
+++ ErpFetch.js
@@ -13,12 +13,13 @@ class ErpFetch {
     this._fetch = transport || fetch
   }
   /**
-   * Make HTTP requests to ressource
-   * @param {String} ressource the resource that you wish to fetch
+   * Make HTTP requests to resource
+   * @param {String} resource the resource that you wish to fetch
    * @param {Object} init custom settings that you want to apply to the request (method, headers ...)
+   * @param {String} responseType 'blob', 'text', 'json', 'arrayBuffer' (json is the default)
    * @returns Promise
    */
-  fetch (ressource, init = {}) {
+  fetch (resource, init = {}, responseType = 'json') {
     init.headers = {
       ...init.headers,
       ...{
@@ -27,94 +28,103 @@ class ErpFetch {
     }
     init.body = JSON.stringify(init.body)
     const request = this._fetch
-    const url = [this.baseUrl, ressource]
-    return request(url.join("/"), init)
-      .then((response) => response.json())
-      .then((data) => {
-        return data
-      })
+    const url = [this.baseUrl, resource]
+    return request(url.join("/"), init).then((response) => {
+      if (responseType === 'blob') {
+        return response.blob()
+      } else if (responseType === 'text') {
+        return response.text()
+      } else if (responseType === 'arrayBuffer') {
+        return response.arrayBuffer()
+      } else {
+        return response.json()
+      }
+    })
   }
 
   /**
    * Post Request to the Rest API
-   * @param {string} ressource API Endpoint
+   * @param {string} resource API Endpoint
    * @param {object} body Body parameters
    * @param {object} init custom settings that you want to apply to the request (method, headers ...)
+   * @param {String} responseType 'blob', 'text', 'json', 'arrayBuffer' (json is the default)
    * @returns Promise
    */
-  post (ressource, body = {}, init = {}) {
+  post (resource, body = {}, init = {}, responseType = 'json') {
     init.headers = {
       ...init.headers,
       ...{
         "Content-Type": "application/json",
       },
     }
-    return this.fetch(ressource, {
+    return this.fetch(resource, {
       ...init,
       ...{ body },
-      ...{ method: "POST" },
-    })
+      ...{ method: "POST" }
+    }, responseType)
   }
 
   /**
    * Put Request to the Rest API
-   * @param {string} ressource API Endpoint
+   * @param {string} resource API Endpoint
    * @param {object} body Body parameters
    * @param {object} init custom settings that you want to apply to the request (headers ...)
+   * @param {String} responseType 'blob', 'text', 'json', 'arrayBuffer' (json is the default)
    * @returns Promise
    */
-  put (ressource, body = {}, init = {}) {
+  put (resource, body = {}, init = {}, responseType = 'json') {
     init.headers = {
       ...init.headers,
       ...{
         "Content-Type": "application/json",
       },
     }
-    return this.fetch(ressource, {
+    return this.fetch(resource, {
       ...init,
       ...{ body },
-      ...{ method: "PUT" },
-    })
+      ...{ method: "PUT" }
+    }, responseType)
   }
 
   /**
    * Post Request to the Rest API
-   * @param {string} ressource API Endpoint
+   * @param {string} resource API Endpoint
    * @param {object} query query string
    * @param {object} init custom settings that you want to apply to the request (headers ...)
+   * @param {String} responseType 'blob', 'text', 'json', 'arrayBuffer' (json is the default)
    * @returns Promise
    */
-  get (ressource, query, init) {
-    let url = ressource
+  get (resource, query, init, responseType = 'json') {
+    let url = resource
     if (typeof query === 'object') {
       const params = new URLSearchParams(query)
       url += "?" + params.toString()
     }
-
     return this.fetch(url, {
       ...init,
       ...{ method: "GET" },
-    })
+    }, responseType)
   }
   /**
    * Delete Request to the Rest API
-   * @param {string} ressource API Endpoint
+   * @param {string} resource API Endpoint
    * @param {object} query query string
    * @param {object} init custom settings that you want to apply to the request (headers ...)
+   * @param {String} responseType 'blob', 'text', 'json', 'arrayBuffer' (json is the default)
    * @returns Promise
    */
-  delete (ressource, body = {}, init = {}) {
+  delete (resource, body = {}, init = {}, responseType = 'json') {
     init.headers = {
       ...init.headers,
       ...{
         "Content-Type": "application/json",
       },
     }
-    return this.fetch(ressource, {
+    return this.fetch(resource, {
       ...init,
       ...{ body },
       ...{ method: "DELETE" },
-    })
+    }, responseType)
   }
 }
 module.exports = ErpFetch
